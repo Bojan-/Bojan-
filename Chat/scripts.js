41657d80e9fef79c0105d434539d0e5da8d300be
_Bojan_ <andrey.moysa@gmail.com>
2015-03-15 17:44:46 +0200

    chat adding

--- Chat/scripts.js
+++ Chat/scripts.js
@@ -1,135 +1,167 @@
-var clickY = -1;
-var flag = 0;
-var start = -1;
-var finish = -1;
+﻿var flag = 0;
+var next_id = 0;
+var id = -1;
+var read = 1;
 
 function run(){
-    var history = document.getElementById('history');
 	var appContainer = document.getElementsByClassName('chat')[0];
-
 	appContainer.addEventListener('click', delegateEvent);
+	var history = JSON.parse(localStorage.getItem("History"));
+	restore(history);
+}
+
+function restore(string) {
+	if (string != null)
+	for (var i = 0; i < string.length; i++)
+	{
+		switch (string[i]) {
+          case "S":
+			i += 2;
+			var n = "";
+			while (string[i] != ":")
+			{
+				n += string[i];
+				i++;
+			}
+			i += 2;
+			var m = "";
+			while (string[i] != "\n")
+			{
+				m += string[i];
+				i++;
+			}
+			var name = document.getElementById('name');
+			var message = document.getElementById('message');
+			name.value = n;
+			message.value = m;
+			SendMessage();
+			break;
+          case "C":
+            i += 2;
+            var _id = "";
+            while (string[i] != " ")
+			{
+				_id += string[i];
+				i++;
+			}
+            id = _id;
+            i++;
+			var me = "";
+			while (string[i] != "\n")
+			{
+				me += string[i];
+				i++;
+			}
+			var _message = document.getElementById('message');
+			_message.value = me;
+			SendChangedMessage();
+			break;
+          case "D":
+            i += 2;
+            var iD = "";
+            while (string[i] != "\n")
+			{
+				iD += string[i];
+				i++;
+			}
+            id = iD;
+            DeleteMessage();
+			break;
+          default:
+			i++;
+		}
+	}
+	read = 0;
 }
 
 function delegateEvent(evtObj) {
 	if(evtObj.type === 'click'){
         var temp = document.getElementById('send');
-            if (evtObj.target == temp) {
-		if (flag == 0)
-		    SendMessage();
+        if (evtObj.target == temp) {
+		if (flag === 0)
+		    	SendMessage();
 		else
-		    SendChangedMessage();
-	    }
-        temp = document.getElementById('history');
-            if (evtObj.target == temp)
-		        clickY = SelectMessage(evtObj);
+		    	SendChangedMessage();
+	}
+        if (evtObj.target.id < next_id)
+		SelectMessage(evtObj);
         temp = document.getElementById('change');
-            if (evtObj.target == temp)
-		        ChangeMessage();
+        if (evtObj.target == temp)
+		ChangeMessage();
         temp = document.getElementById('delete');
-            if (evtObj.target == temp)
-		        DeleteMessage();
+        if (evtObj.target == temp)
+		DeleteMessage();
 	}
 }
 
 function SendMessage(){
  	var history = document.getElementById('history');
 	var message = document.getElementById('message');
-    if(!message.value){
+    if(!message.value) {
 		return;
 	}
-
     var name = document.getElementById('name');
-    if (name.value !== '')
-        history.value = history.value + name.value + ': ' +  message.value + '\n';
+    if (name.value !== '') {
+        var newdiv = document.createElement("div");
+        newdiv.style.fontSize = "x-large";
+        newdiv.innerHTML = name.value + ": " + message.value;
+        newdiv.value = message.value;
+        newdiv.name = name.value;
+        newdiv.id = next_id;
+        next_id++;
+        history.appendChild(newdiv);
+	if (read === 0)
+		localStorage.setItem("History", JSON.stringify(JSON.parse(localStorage.getItem("History")) + "S " + newdiv.innerHTML + "\n"));
+    }        	
     else
-        message.placeholder = "??????? ???!!!";
+        message.placeholder = "Введите имя!!!";
 	message.value = '';
 }
 
 function SelectMessage(evtObj) {
-    var text = document.getElementById('history');
-    var y = (evtObj.pageY - 90) / 25;
-    var i = 0;
-    var count = 0;
-    var count1 = 0;
-    var old = i;
-    while (count1 < y && i < text.value.length) {
-        if (text.value[i] === '\n') {
-            count++;
-	    count1++;
-	}
-	if (i - old === 70) {
-	    count1++;
-	    old = i;
-	}
-        i++;
+    if (id != -1)
+    {
+	var mes = document.getElementById(id);
+	mes.innerHTML = mes.name + ": " + mes.value;
     }
-    if (i != text.value.length)
-    	return count;
-    else
-	return -1;
+    var text = document.getElementById('history');
+	var mes = document.getElementById(evtObj.target.id);
+	mes.innerHTML = "<b>" + mes.innerHTML + "</b>";
+    id = mes.id;
+    document.getElementById('delete').hidden = false;
+    document.getElementById('change').hidden = false;
 }
 
 function DeleteMessage() {
-    if (clickY != -1) {
-    	var text = document.getElementById('history');
-    	var temp = "";
-    	var i = 0;
-    	var j = 1;
-    	var count = 0;
-    	while (count < clickY) {
-            i++;
-            if (text.value[i] === '\n')
-            	count++;
-    	}
-	temp = text.value.substring(0, i);
-    	while (text.value[i+j] != '\n')
-            j++;
-	if (i === 0)
-    	    temp = temp.concat(text.value.substring(i+j+1, text.value.length));
-	else
-	    temp = temp.concat(text.value.substring(i+j, text.value.length));
-    	text.value = temp;
-    }
+    document.getElementById('history').removeChild(document.getElementById(id));
+    document.getElementById('delete').hidden = true;
+    document.getElementById('change').hidden = true;
+    id = -1;
+    if (read === 0)
+    	localStorage.setItem("History", JSON.stringify(JSON.parse(localStorage.getItem("History")) + "D " + id + "\n"));
 }
 
 function ChangeMessage() {
-    if (clickY != -1) {
-	var text = document.getElementById('history');
-    	var temp = "";
-    	var i = 0;
-    	var j = 0;
-	var old = i;
-    	var count = 0;
-    	while (count < clickY) {
-            i++;
-            if (text.value[i] === '\n')
-            	count++;
-    	}
-	while (text.value[i-2] != ':')
-	    i++;
-	while (text.value[i+j+1] != '\n')
-            j++;
-	temp = text.value.substring(i, i+j+1);
-	var message = document.getElementById('message');
-	message.value = temp;
-	start = i+1;
-	finish = j;
-	flag = 1;
-    }
+    var mes = document.getElementById(id);
+    var message = document.getElementById('message');
+    message.value = mes.value;
+    flag = 1;
+    document.getElementById('delete').hidden = true;
+    document.getElementById('change').hidden = true;
 }
+
 function SendChangedMessage() {
-    if (start != -1 && finish != -1) {
-	var text = document.getElementById('history');
-	var message = document.getElementById('message');
-	if (start != 1)
-    	    var temp = text.value.substring(0, start-1);
-	else
-	    var temp = "";
-	temp = temp.concat(message.value);
-	temp = temp.concat(text.value.substring(start+finish, text.value.length));
-	text.value = temp;
-	flag = 0;
-	message.value = '';
+    var message = document.getElementById('message');
+    var mes = document.getElementById(id);
+    if(!message.value) {
+	    mes.innerHTML = mes.name + ": " + mes.value;
     }
+    else {
+    	mes.innerHTML = mes.name + ": " + message.value;
+	if (read === 0)
+        	localStorage.setItem("History", JSON.stringify(JSON.parse(localStorage.getItem("History")) + "C " + id + " " + message.value + "\n"));
+    	flag = 0;
+    	message.value = '';
+    }
+    id = -1;
 }
